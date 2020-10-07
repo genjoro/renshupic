ebec1f7fcdbc31aa8f40aa6e2b63e334c3b9e7ed
genjoro <72503622+genjoro@users.noreply.github.com>
2020-10-07 21:37:48 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('#point li').each(function(){
+		$(this).click(function(){
+			var idname = $(this).attr("id").substring(3).toLowerCase();
+			var classname = '.';
+			classname += idname;
+			$('#hansoku div:not(classname)').css('display','none');
+			$(classname).fadeIn(200);
+		});
+	});
+});
